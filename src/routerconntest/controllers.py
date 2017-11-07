f54890e0530d919bdd1bf30500c8e596b7fbf1c5
KyeonghwanRyu <yolkryu@gmail.com>
2017-11-07 21:14:27 +0900

    route 선택 기능 추가
    + gitignore 추가
    + static/bootstrap

--- src/routerconntest/controllers.py
+++ src/routerconntest/controllers.py
@@ -23,13 +23,16 @@ def respond_update():
     data_route = generate_route()
     json_route = json.dumps(data_route)
 
+    # for debug. have to change after
+    json_routes = json.dumps([json_route, json_route, json_route])
+
     response = app.response_class(
-        response=json_route,
+        response=json_routes,
         status=200,
         mimetype='application/json'
     )
 
-    print('>> Response: %s' % json_route)
+    print('>> Response: %s' % json_routes)
 
     return response
 
